8f7d91b051d1fe8856beaa281871369a3be2f1d4
charmant27 <charmantogatera@gmail.com>
2022-11-26 07:32:07 -0800

    first next.js commit

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h2>about page</h2>
+    </>
+  );
+};
+
+export default about;
